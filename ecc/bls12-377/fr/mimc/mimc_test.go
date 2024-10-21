767df4441aeb83d864dba9133d116a6b56700f63
olegfomenko <oleg.fomenko2002@gmail.com>
2024-10-21 19:26:24 +0300

    adding mimc hash benches

--- ecc/bls12-377/fr/mimc/mimc_test.go
+++ ecc/bls12-377/fr/mimc/mimc_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/crypto/sha3"
 	mrand "math/rand"
 	"testing"
 )
@@ -70,105 +69,35 @@ func TestHashASMMIMC(t *testing.T) {
 func BenchmarkEncryptNative(b *testing.B) {
 	initConstants()
 
-	for i := 0; i < 1000; i++ {
-		m, _ := new(fr.Element).SetRandom()
-		h, _ := new(fr.Element).SetRandom()
-		encryptTest1(h, m)
-	}
-
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		doStuff()
+
 		m, _ := new(fr.Element).SetRandom()
 		h, _ := new(fr.Element).SetRandom()
+
+		d := digest{}
+		d.h = *h
+		msg := *m
 
 		b.StartTimer()
-		encryptTest1(h, m)
-	}
-}
-
-func BenchmarkEncryptNative2(b *testing.B) {
-	initConstants()
-
-	for i := 0; i < 1000; i++ {
-		m, _ := new(fr.Element).SetRandom()
-		h, _ := new(fr.Element).SetRandom()
-
-		msg := *m
-		hash := *h
-		encryptTest2(hash, msg)
-	}
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		doStuff()
-		m, _ := new(fr.Element).SetRandom()
-		h, _ := new(fr.Element).SetRandom()
-
-		msg := *m
-		hash := *h
-		b.StartTimer()
-		encryptTest2(hash, msg)
+		d.encrypt(msg)
 	}
 }
 
 func BenchmarkEncryptASM(b *testing.B) {
-	for i := 0; i < 1000; i++ {
-		m, _ := new(fr.Element).SetRandom()
-		h, _ := new(fr.Element).SetRandom()
-		fr.MIMCEncrypt(h, m)
-	}
+	m, _ := new(fr.Element).SetRandom()
+	h, _ := new(fr.Element).SetRandom()
+	fr.MIMCEncrypt(h, m)
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		doStuff()
 		m, _ := new(fr.Element).SetRandom()
 		h, _ := new(fr.Element).SetRandom()
 		b.StartTimer()
 		fr.MIMCEncrypt(h, m)
-	}
-}
-
-func encryptTest1(h, m *fr.Element) *fr.Element {
-	var tmp fr.Element
-	for i := 0; i < mimcNbRounds; i++ {
-		// m = (m+k+c)^**17
-		tmp.Add(m, h).Add(&tmp, &mimcConstants[i])
-		m.Square(&tmp).
-			Square(m).
-			Square(m).
-			Square(m).
-			Mul(m, &tmp)
-	}
-	m.Add(m, h)
-	return m
-}
-
-func encryptTest2(m, h fr.Element) fr.Element {
-	var tmp fr.Element
-	for i := 0; i < mimcNbRounds; i++ {
-		// m = (m+k+c)^**17
-		tmp.Add(&m, &h).Add(&tmp, &mimcConstants[i])
-		m.Square(&tmp).
-			Square(&m).
-			Square(&m).
-			Square(&m).
-			Mul(&m, &tmp)
-	}
-	m.Add(&m, &h)
-	return m
-}
-
-func doStuff() {
-	for i := 0; i < 50; i++ {
-		tmp := make([]byte, 4096)
-		mrand.Read(tmp)
-		sha3.Sum256(tmp)
 	}
 }
